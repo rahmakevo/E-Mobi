0b6c98c478476925e30e96cd4bf3c743f186856f
rahmakevo <siundu344@gmail.com>
2018-04-09 11:33:51 +0300

    working on start layout

--- app/src/main/java/ke/co/dev/siundu344/emobi/ui/StartActivity.java
+++ app/src/main/java/ke/co/dev/siundu344/emobi/ui/StartActivity.java
@@ -3,6 +3,8 @@ package ke.co.dev.siundu344.emobi.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ke.co.dev.siundu344.emobi.R;
+
 public class StartActivity extends AppCompatActivity {
 
     @Override
